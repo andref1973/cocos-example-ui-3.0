cb4fd53a074c5252796a4956a1188ebd09fb7df2
andref1973 <105914411+andref1973@users.noreply.github.com>
2022-05-27 11:12:52 +0800

    first commit

---
+++ cocos-js/wait-for-ammo-instantiation.js
@@ -0,0 +1,14 @@
+System.register(['./instantiated-0570fc81.js'], function (exports) {
+	'use strict';
+	return {
+		setters: [function (module) {
+			exports('default', module.h5);
+		}],
+		execute: function () {
+
+
+
+		}
+	};
+});
+//# sourceMappingURL=wait-for-ammo-instantiation.js.map
